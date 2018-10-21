43953547ebdc00ba98b00508fc002565ff459737
francotesei <ftesei96@gmail.com>
2018-10-21 15:38:15 -0300

    new profile

---
+++ precache-manifest.848e895dc8439e0575b9abad559eb9fd.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "77eebff0c6d1fc7b92a35b058819f24c",
+    "url": "/static/media/docker.77eebff0.png"
+  },
+  {
+    "revision": "1f3b4b780b7adbf68fc9",
+    "url": "/static/css/main.d4c090d9.chunk.css"
+  },
+  {
+    "revision": "55f9fb07af280cee84fb",
+    "url": "/static/js/1.55f9fb07.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9e2f38d8c48be968314837c105c86483",
+    "url": "/static/media/angular2.9e2f38d8.png"
+  },
+  {
+    "revision": "02bc6746f45e496f31182bff26bef339",
+    "url": "/static/media/elm.02bc6746.png"
+  },
+  {
+    "revision": "426671625b9c84e7ae1a7d25a13ebd38",
+    "url": "/static/media/java.42667162.png"
+  },
+  {
+    "revision": "773fbe1058e97eef66136df341848f35",
+    "url": "/static/media/scala.773fbe10.png"
+  },
+  {
+    "revision": "0e28e3aeaf2628137fce93b366aeda2c",
+    "url": "/static/media/python.0e28e3ae.png"
+  },
+  {
+    "revision": "0f32a8df8955389f02c9c0dea3489fc0",
+    "url": "/static/media/cplusplus.0f32a8df.png"
+  },
+  {
+    "revision": "8d6799600538d2be26325e2d0e7cdf3c",
+    "url": "/static/media/react.8d679960.png"
+  },
+  {
+    "revision": "1f2d6ef6ecdcf8f43715b0db09a6d5f5",
+    "url": "/static/media/redux.1f2d6ef6.png"
+  },
+  {
+    "revision": "7102554897aeeb5fe054ae3080d10975",
+    "url": "/static/media/springboot.71025548.png"
+  },
+  {
+    "revision": "1f3b4b780b7adbf68fc9",
+    "url": "/static/js/main.1f3b4b78.chunk.js"
+  },
+  {
+    "revision": "dbf2390d8416171973b2abff6a64a9bf",
+    "url": "/static/media/kube.dbf2390d.png"
+  },
+  {
+    "revision": "43e38e5b1f4ade762987397b012039a3",
+    "url": "/static/media/kafka.43e38e5b.png"
+  },
+  {
+    "revision": "f8ef49b18d5bd69e5ebdfbccd82bdd3b",
+    "url": "/static/media/spark.f8ef49b1.png"
+  },
+  {
+    "revision": "fe039bd6866a2ac85714a8726cc5ece1",
+    "url": "/static/media/jupyter.fe039bd6.png"
+  },
+  {
+    "revision": "a17181a7e2c5ee7ebce66b110b24e4f0",
+    "url": "/static/media/redisdb.a17181a7.png"
+  },
+  {
+    "revision": "c31709341a21f028809f3bb992b10783",
+    "url": "/static/media/postgresdb.c3170934.png"
+  },
+  {
+    "revision": "73b49f9a10b0660ac89174646a480ab9",
+    "url": "/static/media/bootstrap.73b49f9a.png"
+  },
+  {
+    "revision": "ef21031665a890b883e3ca2bf7f17e7e",
+    "url": "/static/media/ionic2.ef210316.png"
+  },
+  {
+    "revision": "7003e68a7f80c7a24b553d1e82ac33f9",
+    "url": "/static/media/jenkins.7003e68a.png"
+  },
+  {
+    "revision": "a08f59f44e32ab5f5fe579b7b4088b97",
+    "url": "/static/media/linkedin.a08f59f4.png"
+  },
+  {
+    "revision": "d3bc903cc9299df003ce3103bad39c34",
+    "url": "/static/media/gmail.d3bc903c.svg"
+  },
+  {
+    "revision": "be5405ef318a0decc912373b24515a89",
+    "url": "/index.html"
+  }
+];
